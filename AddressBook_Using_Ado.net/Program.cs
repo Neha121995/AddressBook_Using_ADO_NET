c379c2cfb0446867e4f17fb858d9d6abad82f5df
Neha121995 <jagtapneha1212@gmail.com>
2021-08-18 21:57:08 +0530

    [Neha] Added: Address Book Problem Using ADO.NET

--- AddressBook_Using_Ado.net/Program.cs
+++ AddressBook_Using_Ado.net/Program.cs
@@ -7,6 +7,93 @@ namespace AddressBook_Using_Ado.net
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book using ADO.NET");
+
+            AddressBookRepo objrepo = new AddressBookRepo();
+            //abrepo.CheckConnection();
+            AddressBookModel objmodel = new AddressBookModel();
+            AddressBookModel objUpdatemodel = new AddressBookModel();
+            AddressBookModel objdelmodel = new AddressBookModel();
+            while (true)
+            {
+                Console.WriteLine("\nEnter Choice  \n1.AddContact \n2.EditContact \n3.DeleteContact \n4.RetriveStateorCity" +
+                                  "\n5.SizeofBook\n6.SortPersonNameByCity\n7.identifyEachAddressbook\n8.CountByBookType\n9.Exit ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            //AddressBookModel abmodel = new AddressBookModel();
+                            objmodel.First_Name = "Neha";
+                            objmodel.Last_Name = "Jagtap";
+                            objmodel.Address = "Shahunagar";
+                            objmodel.City = "Satara";
+                            objmodel.State = "Maharashtra";
+                            objmodel.Zip = "415001";
+                            objmodel.Phone_Number = "7498753245";
+                            objmodel.Email = "neha@gmail.com";
+                            objmodel.BookName = "MyBook";
+                            objmodel.AddressbookType = "family";
+                            bool result = objrepo.AddContact(objmodel);
+
+                            if (result)
+                                Console.WriteLine("Record added successfully...");
+                            else
+                                Console.WriteLine("Some problem is there...");
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            // Update recods
+                            //AddressBookModel abmodel1 = new AddressBookModel();
+                            objUpdatemodel.First_Name = "Nikita";
+                            objUpdatemodel.Last_Name = "Jadhav";
+                            objUpdatemodel.City = "Satara";
+                            objUpdatemodel.State = "Maharashtra";
+                            objUpdatemodel.Email = "nikita@gmail.com";
+                            objUpdatemodel.BookName = "MyBook1";
+                            objUpdatemodel.AddressbookType = "Family1";
+                            objrepo.EditContactUsingFirstName(objUpdatemodel);
+                            Console.ReadKey();
+                            break;
+                        case 3:
+                            objdelmodel.First_Name = "Neha";
+                            objrepo.DeleteContactUsingName(objdelmodel);
+                            Console.ReadKey();
+                            break;
+                        case 4:
+                            objrepo.RetrieveContactFromPerticularCityOrState();
+                            Console.ReadKey();
+                            break;
+                        case 5:
+                            objrepo.AddressBookSizeByCityANDState();
+                            Console.ReadKey();
+                            break;
+                        case 6:
+                            objrepo.SortPersonNameByCity();
+                            Console.ReadKey();
+                            break;
+                        case 7:
+                            objrepo.identifyEachAddressbooktype();
+                            Console.ReadKey();
+                            break;
+                        case 8:
+                            objrepo.GetNumberOfContactsCountByBookType();
+                            Console.ReadKey();
+                            break;
+
+                        case 9:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Please enter the valid choise :: ");
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Only enter integer number :: ");
+                }
+            }
         }
     }
 }
---
+++ AddressBook_Using_Ado.net/SQLQuery1.sql
@@ -0,0 +1,42 @@
+﻿--UC1 : create a new db for address book----
+create database addressBook;
+
+--use db for address book----
+use addressBook;
+
+--UC2 : Create table for address book----
+create table addressBook
+(
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+address varchar(100) not null,
+city varchar(20) not null,
+state varchar(50) not null,
+zip varchar(6) not null,
+phone_number varchar(11) not null,
+email varchar(50) not null,
+bookname varchar(100) ,
+addressbooktype varchar(50)
+);
+
+--Display table for address book----
+select * from addressBook;
+
+CREATE PROCEDURE addressBookProcedure
+@first_name varchar(50) ,
+@last_name varchar(50) ,
+@address varchar(100),
+@city varchar(20),
+@state varchar(50),
+@zip varchar(6),
+@phone_number varchar(11),
+@email varchar(50),
+@bookname varchar(100) ,
+@addressbooktype varchar(50)
+as
+insert into addressBook values(@first_name,@last_name,@address,@city,@state,@zip,@phone_number,
+    @email,@bookname,@addressbooktype);
+go
+
+select * from addressBook;
+
